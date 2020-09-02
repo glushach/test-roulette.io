2f5d707140ab41802c4defd75c2f46a22c87f198
Aleksandr <shoppingplaneta@gmail.com>
2020-09-02 18:20:47 +0300

    Made slider

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
+  //Spoiler
   $('.header-title').click(function(event){
     $(this).toggleClass('active').next().slideToggle(300);
+  });
+  $('.slider-for').slick({
+    asNavFor: '.slider-nav',
+    arrows: false,
+    autoplay: true, //тянет и нижные слайдеры
+    draggable: false,
+    swipe: false,
+    fade: true,
+  });
+  $('.slider-nav').slick({
+    asNavFor: '.slider-for',
+    arrows: false,
+    draggable: false,
+    swipe: false,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          variableWidth: false,
+        }
+      }
+    ]
   });
 });
